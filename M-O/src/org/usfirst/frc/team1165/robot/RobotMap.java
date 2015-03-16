894c956cb1cce007431c848f4dc76efeb0b7523c
Sudarshan1999 <k.b.sudarshan@gmail.com>
2015-03-15 19:22:09 -0700

    New commands / auto routines from recycle rush to MO

--- M-O/src/org/usfirst/frc/team1165/robot/RobotMap.java
+++ M-O/src/org/usfirst/frc/team1165/robot/RobotMap.java
@@ -13,6 +13,7 @@ public class RobotMap
 
     public static final double DRIVE_SPEED 							=   1;		// Drive magnitude
     public static final double DRIVE_START_SPEED 					=   1;		// Drive magnitude
+    public static final double DRIVE_CREEP_SPEED 	 =   0.2;	// Drive Creep magnitude
     public static final double ROTATE_SPEED 						=   0.4;	// Rotate magnitude
     public static final double ROTATE_CREEP_SPEED 					=   0.2;	// Rotate magnitude
     public static final double PICKUP_WHEELS_SPEED 					=   1;		// Wheel magitude
---
+++ M-O/src/org/usfirst/frc/team1165/robot/commands/MoveRobotToteAndContainerToAutoZone.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team1165.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+import org.usfirst.frc.team1165.robot.RobotMap;
+
+/**
+ *
+ */
+public class MoveRobotToteAndContainerToAutoZone extends CommandGroup
+{
+
+	public MoveRobotToteAndContainerToAutoZone()
+	{
+		addSequential(new PickupTote());
+		//addSequential(new WaitCommand(0.8));
+		addSequential(new MovePickupArmsUp());
+		addSequential(new RotateToHeading(RobotMap.ROTATE_SPEED, 15, 90, RobotMap.ROTATE_CREEP_SPEED));
+		addSequential(new WaitCommand(RobotMap.WAIT_FOR_SUBSYSTEM_TO_COMPLETE)); //give gyro and encoder a chance to reset
+		addSequential(new DriveStraightDistance(0.5 , RobotMap.DISTANCE_TO_AUTO_ZONE ));
+	}
+}
